c2ab274ec131631e6146a085f6656875eebec82d
ShanySB <shanyb.sb@gmail.com>
2025-08-18 21:52:35 +0100

    updated js to display questions better

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,25 +1,46 @@
 $(document).ready(function () {
+    let choices = [];
+
     $('.clickable-card').click(function () {
-        var currentSet = $(this).closest('.image-set');
-        var nextSet = currentSet.next('.image-set');
+        let currentSet = $(this).closest('.image-set');
+        let nextSet = currentSet.next('.image-set');
+        let chosenImg = $(this).find('img').attr('src');
+
+        // Save the chosen card
+        choices.push(chosenImg);
 
         if (nextSet.length) {
+            // Go to the Next set of imgs
             currentSet.fadeOut(500, function () {
                 currentSet.removeClass('active');
                 nextSet.fadeIn(500).addClass('active');
-
-                // If there's no more next set, show reset button
-                if (nextSet.next('.image-set').length === 0) {
-                    $('#reset-btn').fadeIn();
-                }
             });
+        } else {
+            // No more sets = show results
+            currentSet.fadeOut(500, function () {
+                // Show chosen cards
+                choices.forEach(src => {
+                    $('#chosen-cards').append(`
+                        <div class="col-md-3 mb-3">
+                            <img src="${src}" class="img-fluid rounded border">
+                        </div>
+                    `);
+                });
+            });
+
+            $('#results').fadeIn(500);
         }
     });
 
     // Reset button functionality
     $('#reset-btn').click(function () {
-        $('.image-set').fadeOut(500).removeClass('active');
-        $('.image-set').first().fadeIn(500).addClass('active');
-        $(this).fadeOut();
+        choices = [];
+        $('#chosen-cards').empty();
+        $('#results').fadeOut(500, function () {
+            $('.image-set').hide().removeClass('active');
+            $('.image-set').first().fadeIn(500).addClass('active');
+        });
     });
 });
+
+    
