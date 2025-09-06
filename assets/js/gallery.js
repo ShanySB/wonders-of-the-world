c578104f4701c0732b968c840aa36905169f64d8
ShanySB <shanyb.sb@gmail.com>
2025-09-06 23:34:32 +0100

    added modal interactivity

---
+++ assets/js/gallery.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    /**
+     * Popup modal functionality
+     */
+  $(".place-img").click(function () {
+    let country = $(this).data("country");
+
+    // Update modal title
+    $("#modal-title").text(country);
+
+    // Fetch summary from Wikipedia API
+    $.getJSON(`https://en.wikipedia.org/api/rest_v1/page/summary/${country}`, function (data) {
+      $("#modal-body").html(`
+        <p>${data.extract}</p>
+        <a href="${data.content_urls.desktop.page}" target="_blank">Read more on Wikipedia</a>
+      `);
+    }).fail(function () {
+      $("#modal-body").text("Sorry, no information found.");
+    });
+
+    // Show the modal
+    $("#infoModal").modal("show");
+  });
+});
